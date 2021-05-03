ec23e61afdafe2705b680b637ad433919f682472
Warren Wilson <warrenwilson77@gmail.com>
2021-05-03 19:01:14 -0400

    addressed architectural problems regarding App.js (poorly written components)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import GetPrice from "./app/findPrice";
 import "./styles/app.scss";
+import Calculate from "./app/calculate.js";
 
 function App() {
   return (
@@ -31,72 +31,8 @@ function App() {
       </div>
 
       <h3 className="h3">Select Option</h3>
-      <div className="option">
-        <form class="form-container">
-          <button className="button" type="button">
-            Find Option
-          </button>
 
-          <div className="side">
-            <label className="inputLabel"> Side </label>
-
-            <div className="radio-item">
-              <input className="radio-1" type="radio" />
-              <label>Buy </label>
-            </div>
-
-            <div className="radio-item">
-              <input className="radio-1" type="radio" />
-              <label>Sell </label>
-            </div>
-          </div>
-
-          <div className="type">
-            <label className="inputLabel"> Type </label>
-
-            <div className="radio-item">
-              <input className="radio-2" type="radio" />
-              <label>Call </label>
-            </div>
-
-            <div className="radio-item">
-              <input className="radio-2" type="radio" />
-              <label>Put </label>
-            </div>
-          </div>
-
-          <div className="quantity">
-            <label className="inputLabel"> Quantity </label>
-            <input className="form" type="text" placeholder="32" />
-          </div>
-        </form>
-
-        <div className="strike-expiry">
-          <div>
-            <label className="inputLabel">Price per option</label>
-            <input className="form" type="form" placeholder="25.48" />
-          </div>
-
-          <div>
-            <label className="inputLabel"> Strike Price</label>
-            <input className="form" type="form" placeholder="79.98" />
-          </div>
-
-          <div>
-            <label className="inputLabel">Date of Expiration</label>
-            <input className="date_form" type="date" />
-          </div>
-
-          {/* <label className="inputLabel"> IV (implied volatility): </label>
-          <input className="form" type="form" placeholder="45.723" /> */}
-        </div>
-      </div>
-      <div>
-        <button className="calculate_button" type="button">
-          Calculate
-        </button>
-      </div>
-      <GetPrice />
+      <Calculate />
     </div>
   );
 }
